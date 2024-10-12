a00ed566d4ad41e31d5d283021a5b66e6d0f7767
Jack <exploretheinternet32@gmail.com>
2024-10-12 17:35:30 +0100

    Character controller completely redone
    movement now resembles that found in source engine games, with seperate air acceleration and ground acceleration

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class FirstPersonController : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed;
+    public float groundAcceleration;
+    public float airAcceleration;
     public float gravity;
     public float maxSpeed;
     public float maxFallSpeed;
+    public float jumpHeight;
     public Vector3 velocity = Vector3.zero;
 
     public bool grounded;
@@ -28,21 +31,44 @@ public class FirstPersonController : MonoBehaviour
     {
         grounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
 
-        float moveX = Input.GetAxisRaw("Horizontal") * speed;
-        float moveY = Input.GetAxisRaw("Vertical") * speed;
+        //Acceleration & deceleration
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (grounded) velocity.x += groundAcceleration * Mathf.Sign(Input.GetAxisRaw("Horizontal"));
+            else velocity.x += airAcceleration * Mathf.Sign(Input.GetAxisRaw("Horizontal"));
+        }
+        else if (grounded)
+        {
+            //Make sure we don't overshoot when doing deceleration 
+            if (Mathf.Sign(velocity.x - groundAcceleration * Mathf.Sign(velocity.x)) != Mathf.Sign(velocity.x)) velocity.x = 0f;
+            else velocity.x -= groundAcceleration * Mathf.Sign(velocity.x);
+        }
 
-        Vector3 moveVector = transform.right * moveX + transform.forward * moveY;
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            if (grounded) velocity.z += groundAcceleration * Mathf.Sign(Input.GetAxisRaw("Vertical"));
+            else velocity.z += airAcceleration * Mathf.Sign(Input.GetAxisRaw("Vertical"));
+        }
+        else if (grounded)
+        {
+            //Make sure we don't overshoot when doing deceleration 
+            if (Mathf.Sign(velocity.z - groundAcceleration * Mathf.Sign(velocity.z)) != Mathf.Sign(velocity.z)) velocity.z = 0f;
+            else velocity.z -= groundAcceleration * Mathf.Sign(velocity.z);
+        }
 
-        if(!grounded) moveVector.y -= gravity;
 
+        if (!grounded) velocity.y += gravity * -9.81f * Time.deltaTime;
+        if (grounded) velocity.y = 0f;
 
+        if (Input.GetButton("Jump") && grounded) velocity.y = jumpHeight;
 
-        moveVector.x = Mathf.Clamp(moveVector.x, -maxSpeed, maxSpeed);
-        moveVector.y = Mathf.Clamp(moveVector.y, -maxFallSpeed, 999);
-        moveVector.z = Mathf.Clamp(moveVector.z, -maxSpeed, maxSpeed);
+        velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
+        velocity.y = Mathf.Clamp(velocity.y, -maxFallSpeed, 999);
+        velocity.z = Mathf.Clamp(velocity.z, -maxSpeed, maxSpeed);
 
-        velocity = moveVector;
+        //Apply the rotation to the velocity
+        Vector3 move = transform.rotation * velocity;
 
-        controller.Move(moveVector * Time.deltaTime);
+        controller.Move(move * Time.deltaTime);
     }
 }
