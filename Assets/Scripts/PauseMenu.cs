855c96fd433f2b96dc946fe04f7f7cbfa2179e1d
Dastardly11 <D.leigh6@edu.salford.ac.uk>
2024-10-21 17:23:42 +0100

    Added pause functionality and made pause menu toggle open when "P" is pressed

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public bool gameIsPaused;
     public CharacterController controller;
+    public GameObject PausePanel;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,16 +16,29 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Escape"))
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            Time.timeScale = 0;
-            bool
-
+            Debug.Log("Paused Pressed");
+            gameIsPaused = !gameIsPaused;
+            PauseGame();       
         }
 
     }
-    public void Resume()
+    public void PauseGame()
     {
-        Time.timeScale = 1;
+        if (gameIsPaused == true)
+        {
+            Debug.Log("Paused");
+            PausePanel.SetActive(true);
+            controller.enabled = false;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Debug.Log("Unpaused");
+            PausePanel.SetActive(false);
+            controller.enabled = true;
+            Time.timeScale = 1;
+        }
     }
 }
