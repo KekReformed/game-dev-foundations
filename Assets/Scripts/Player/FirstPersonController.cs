b169ccb5be42adee540f5461c99b1dbe0ad137c6
Jack <exploretheinternet32@gmail.com>
2024-10-27 16:00:34 +0000

    First person controller fixed,
    No longer can you turn midair with just the mouse

--- Assets/Scripts/Player/FirstPersonController.cs
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -9,7 +9,8 @@ public class FirstPersonController : MonoBehaviour
     public float maxSpeed;
     public float maxFallSpeed;
     public float jumpHeight;
-    public Vector3 velocity = Vector3.zero;
+    public Vector3 inputVelocity = Vector3.zero;
+    public Vector3 move = Vector3.zero;
 
     public bool grounded;
     public Transform groundCheck;
@@ -28,46 +29,63 @@ public class FirstPersonController : MonoBehaviour
     {
 
         //Grounded check, used for jumping and gravity, make sure that if we are moving up that we can't be grounded otherwise we'll get stuck in the floor
-        grounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer) && velocity.y <= 0;
+        grounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer) && move.y <= 0;
 
         //Acceleration & deceleration
         if (Input.GetAxisRaw("Horizontal") != 0)
         {
-            if (grounded) velocity.x += groundAcceleration * Mathf.Sign(Input.GetAxisRaw("Horizontal"));
-            else velocity.x += airAcceleration * Mathf.Sign(Input.GetAxisRaw("Horizontal"));
+            if (grounded) inputVelocity.x += groundAcceleration * Mathf.Sign(Input.GetAxisRaw("Horizontal"));
+            else inputVelocity.x += airAcceleration * Mathf.Sign(Input.GetAxisRaw("Horizontal"));
             //Apply the rotation to the velocity
         }
         else if (grounded)
         {
             //Make sure we don't overshoot when doing deceleration 
-            if (Mathf.Sign(velocity.x - groundAcceleration * Mathf.Sign(velocity.x)) != Mathf.Sign(velocity.x)) velocity.x = 0f;
-            else velocity.x -= groundAcceleration * Mathf.Sign(velocity.x);
+            if (Mathf.Sign(move.x - groundAcceleration * Mathf.Sign(move.x)) != Mathf.Sign(move.x)) move.x = 0f;
+            else move.x -= groundAcceleration * Mathf.Sign(move.x);
+
+            inputVelocity.x = 0;
         }
 
         if (Input.GetAxisRaw("Vertical") != 0)
         {
-            if (grounded) velocity.z += groundAcceleration * Mathf.Sign(Input.GetAxisRaw("Vertical"));
-            else velocity.z += airAcceleration * Mathf.Sign(Input.GetAxisRaw("Vertical"));
+            if (grounded) inputVelocity.z += groundAcceleration * Mathf.Sign(Input.GetAxisRaw("Vertical"));
+            else inputVelocity.z += airAcceleration * Mathf.Sign(Input.GetAxisRaw("Vertical"));
         }
         else if (grounded)
         {
             //Make sure we don't overshoot when doing deceleration 
-            if (Mathf.Sign(velocity.z - groundAcceleration * Mathf.Sign(velocity.z)) != Mathf.Sign(velocity.z)) velocity.z = 0f;
-            else velocity.z -= groundAcceleration * Mathf.Sign(velocity.z);
+            if (Mathf.Sign(move.z - groundAcceleration * Mathf.Sign(move.z)) != Mathf.Sign(move.z)) move.z = 0f;
+            else move.z -= groundAcceleration * Mathf.Sign(move.z);
+
+            inputVelocity.z = 0;
         }
 
 
-        if (!grounded) velocity.y += gravity * -9.81f * Time.deltaTime;
-        if (grounded) velocity.y = 0f;
+        if (!grounded) move.y += gravity * -9.81f * Time.deltaTime;
+        if (grounded) move.y = 0f;
 
-        if (InputManager.instance.inputs["Jump"].pressed && grounded) velocity.y = jumpHeight;
+        if (InputManager.instance.inputs["Jump"].pressed && grounded) move.y = jumpHeight;
 
-        velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
-        velocity.y = Mathf.Clamp(velocity.y, -maxFallSpeed, 999);
-        velocity.z = Mathf.Clamp(velocity.z, -maxSpeed, maxSpeed);
+        inputVelocity = clampVector(inputVelocity);
 
-        Vector3 move = transform.rotation * velocity;
+        //Don't let the input velocity mess with our y vector, this could be done without a temp variable of course but it looks significantly worse
+        float tempMoveY = move.y;
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) move = transform.rotation * inputVelocity;
+
+        move.y = tempMoveY;
 
         controller.Move(move * Time.deltaTime);
+    }
+
+    Vector3 clampVector(Vector3 vectorToClamp)
+    {
+        Vector3 tempVector;
+        tempVector.x = Mathf.Clamp(vectorToClamp.x, -maxSpeed, maxSpeed);
+        tempVector.y = Mathf.Clamp(vectorToClamp.y, -maxFallSpeed, 999);
+        tempVector.z = Mathf.Clamp(vectorToClamp.z, -maxSpeed, maxSpeed);
+
+        return tempVector;
     }
 }
